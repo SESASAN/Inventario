c6fff005c73f92dd534455fd6863725fc2191b82
SEBASTIAN JESUS PEREZ ARAUJO <sebastianjesus.perez@gmail.com>
2024-08-31 14:49:09 -0500

    Creación de Proveedores

---
+++ Bodegas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Bodegas
+{
+    private int id_bodega = 0;
+    private string fecha = "";
+    private int cantidad = 0;
+    private int valor_total = 0;
+
+    public string Id_bodega { get => this.id_bodega; set => this.id_bodega = value; }
+    public string Fecha { get => this.fecha; set => this.fecha = value; }
+    public string Cantidad { get => this.cantidad; set => this.cantidad = value; }
+    public string Valor_total { get => this.valor_total; set => this.valor_total = value; }
+
+    public Bodegas)
+	{
+	}
+}
---
+++ Sucursales.cs
@@ -0,0 +1,17 @@
+﻿public class Sucursales
+{
+    private int id_sucursal = 0;
+    private Bodega? bodega = null;
+    private string nombre = "";
+    private string direccion = "";
+
+
+    public int Id_sucursal { get => this.id_sucursal; set => this.id_sucursal = value; }
+    public Bodega? Bodega { get => this.bodega; set => this.bodega = value; }
+    public string Nombre { get => this.Nombre; set => this.Nombre = value; }
+    public string Direccion { get => this.direccion; set => this.direccion = value; }
+
+    public Sucursales()
+	{
+	}
+}
