0bfabeb512f9755a467a1356d4c16fe96438031b
Samuel Quiroz Rincón <samuelquirozrincon@gmail.com>
2024-09-03 18:35:38 -0500

    Se corrige una relación en el código y los nombres de los diagramas

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -67,7 +67,7 @@ public class Sucursales
     public Sucursales() { }
 
     private int id_sucursal = 0;
-    private Bodegas? bodega = null;
+    private Bodegas bodega = new Bodegas();
     private string nombre = "";
     private string direccion = "";
 
@@ -79,24 +79,17 @@ public class Sucursales
     public void calcularPrecioSucursal() { }
 }
 
-// Creamos la clase Categorías
 public class Categorias
 {
     public Categorias() { }
-    /* Se hace uso del enum para poder especificar si la categoría que 
-    vamos a usar es para la sección de los estantes o el tipo de producto*/
     public enum Grupos { PRODUCTO = 0, SECCION = 1 }
 
-    // Se crea la id de la categoría
     private int id = 0;
 
-    // Se crea la variable del nombre de la categoría
     private string nombre = "";
 
-    // Se crea la variable del grupo que automaticamente se pone como default el grupo de Productos
     private int grupo = (int)Grupos.PRODUCTO;
 
-    // Se crean los getters y setters de dichos atributos de la clase
     public int Id { get => this.id; set => this.id = value; }
     public string Nombre { get => this.nombre; set => this.nombre = value; }
     public int Grupo { get => this.grupo; set => this.grupo = value; }
