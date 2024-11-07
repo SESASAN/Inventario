7d0a16336381a5e53219654e7026fd5ca2fe22b6
JuanMedi <juanjosemedinasepulveda@gmail.com>
2024-11-06 22:23:02 -0500

    AGREGADA CLASE COMUNICACIONES

---
+++ lib_comunicaciones/Comunicaciones.cs
@@ -0,0 +1,135 @@
+﻿using lib_utilidades;
+using System.Text.Json.Serialization;
+
+namespace lib_comunicaciones
+{
+    public class Comunicaciones
+    {
+        public string? Protocolo = "http://",//es el tipo de url que se usa normalmente es http o https pero el S pide certificados y eso
+            Host = "localhost:5056",//nombre del servidor depende de cada servidor
+            Servicio = "",// "asp_notas_servicios/",//esto se pone cuando se publica en el IIS pero como no hay IIS se deja vacia
+            Nombre = string.Empty,//nombre de la enidad controlador 
+            Final = string.Empty,
+            token = null;
+
+        public Comunicaciones(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public Dictionary<string, object> BuildUrl(Dictionary<string, object> data, string Metodo)
+        {
+            data["Url"] = Protocolo + Host + "/" + Servicio + Nombre + "/" + Metodo + Final;
+            data["UrlToken"] = Protocolo + Host + "/" + Servicio + "Token/Autenticar" + Final;
+            return data;
+        }
+
+        public async Task<Dictionary<string, object>> Execute(Dictionary<string, object> datos)
+        {
+            var respuesta = new Dictionary<string, object>();
+            try
+            {
+                respuesta = await Authenticate(datos);
+                if (respuesta == null || respuesta.ContainsKey("Error"))
+                    return respuesta!;
+                respuesta.Clear();
+
+                var url = datos["Url"].ToString();
+                datos.Remove("Url");
+                datos.Remove("UrlToken");
+                datos["Bearer"] = token!;
+                var stringData = JsonConversor.ConvertirAString(datos);//el json conversor convierte el diccionacrio a string y porque el diccionario solo lo entiende el C# o codigo
+                //NOTA: LOS SERVICIOS POR MADURES MANDA CODIGOS SI ES DE LA 200 a 299 es ok si es 300 a 399 es que falla algo para nosotros siempre va a ser 200 pq llegara el json con el error
+                var httpClient = new HttpClient();
+                httpClient.Timeout = new TimeSpan(0, 4, 0);
+
+                var message = await httpClient.PostAsync(url, new StringContent(stringData));
+
+                if (!message.IsSuccessStatusCode)
+                {
+                    respuesta.Add("Error", "lbErrorComunicacion");
+                    return respuesta;
+                }
+
+                var resp = await message.Content.ReadAsStringAsync();
+                httpClient.Dispose(); httpClient = null;
+
+                if (string.IsNullOrEmpty(resp))
+                {
+                    respuesta.Add("Error", "lbErrorAutenticacion");
+                    return respuesta;
+                }
+                resp = Replace(resp);
+                respuesta = JsonConversor.ConvertirAObjeto(resp);
+                return respuesta;
+            }
+            catch (Exception ex)
+            {
+                respuesta["Error"] = ex.ToString();
+                return respuesta;
+            }
+        }
+
+        private async Task<Dictionary<string, object>> Authenticate(Dictionary<string, object> datos)
+        {
+            var respuesta = new Dictionary<string, object>();
+            try
+            {
+                var url = datos["UrlToken"].ToString();
+                var temp = new Dictionary<string, object>();
+                temp["Usuario"] = DatosGenerales.usuario_datos;
+                var stringData = JsonConversor.ConvertirAString(temp);
+
+                var httpClient = new HttpClient();
+                httpClient.Timeout = new TimeSpan(0, 1, 0);
+                var mensaje = await httpClient.PostAsync(url, new StringContent(stringData));
+
+                if (!mensaje.IsSuccessStatusCode)
+                {
+                    respuesta.Add("Error", "lbErrorComunicacion");
+                    return respuesta;
+                }
+
+                var resp = await mensaje.Content.ReadAsStringAsync();
+                httpClient.Dispose(); httpClient = null;
+                if (string.IsNullOrEmpty(resp))
+                {
+                    respuesta.Add("Error", "lbErrorAutenticacion");
+                    return respuesta;
+                }
+
+                resp = Replace(resp);
+                respuesta = JsonConversor.ConvertirAObjeto(resp);
+                token = respuesta["Token"].ToString();
+                return respuesta;
+            }
+            catch (Exception ex)
+            {
+                respuesta["Error"] = ex.ToString();
+                return respuesta;
+            }
+        }
+
+        private string Replace(string resp)//remplaza cosas malas que puede traer el json como primero mandar un " y luego un ' este lo rempalaza para que no salga error
+        {
+            return resp.Replace("\\\\r\\\\n", "")
+                .Replace("\\r\\n", "")
+                .Replace("\\", "")
+                .Replace("\\\"", "\"")
+                .Replace("\"", "'")
+                .Replace("'[", "[")
+                .Replace("]'", "]")
+                .Replace("'{'", "{'")
+                .Replace("\\\\", "\\")
+                .Replace("'}'", "'}")
+                .Replace("}'", "}")
+                .Replace("\\n", "")
+                .Replace("\\r", "")
+                .Replace("    ", "")
+                .Replace("'{", "{")
+                .Replace("\"", "")
+                .Replace("  ", "")
+                .Replace("null", "''");
+        }
+    }
+}
--- lib_utilidades/obj/Debug/net8.0/lib_utilidades.AssemblyInfo.cs
+++ lib_utilidades/obj/Debug/net8.0/lib_utilidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_utilidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5268e8838891270b8cab1bc32f7731fd920af36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65801b4e9ecd8a7010bb53a12b960f3e11ce8eeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_utilidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_utilidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
