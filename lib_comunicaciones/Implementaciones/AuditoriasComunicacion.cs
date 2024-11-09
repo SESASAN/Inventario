e82da135a7537e531c9ee571240dd4e0107e579e
JuanMedi <juanjosemedinasepulveda@gmail.com>
2024-11-08 22:03:42 -0500

    AGREGADAS NUEVAS ENTIDADES A LIB_COMUNICACIONES

---
+++ lib_comunicaciones/Implementaciones/AuditoriasComunicacion.cs
@@ -0,0 +1,33 @@
+﻿using lib_comunicaciones.Interfaces;
+
+namespace lib_comunicaciones.Implementaciones
+{
+    public class AuditoriasComunicacion : IAuditoriasComunicacion
+    {
+        private Comunicaciones? comunicaciones = null;
+        private string? Nombre = "Auditorias";
+        //Esto garantiza que por cada accion pedida es un objeto nuevo 
+        public AuditoriasComunicacion()
+        {
+            comunicaciones = new Comunicaciones(Nombre);
+        }
+
+        public async Task<Dictionary<string, object>> Guardar(Dictionary<string, object> datos)
+        {
+            datos = comunicaciones!.BuildUrl(datos, "Guardar");
+            return await comunicaciones!.Execute(datos);
+        }
+
+        public async Task<Dictionary<string, object>> Buscar(Dictionary<string, object> datos)
+        {
+            datos = comunicaciones!.BuildUrl(datos, "Buscar");
+            return await comunicaciones!.Execute(datos);
+        }
+
+        public async Task<Dictionary<string, object>> Listar(Dictionary<string, object> datos)
+        {
+            datos = comunicaciones!.BuildUrl(datos, "Listar");
+            return await comunicaciones!.Execute(datos);
+        }
+    }
+}
---
+++ lib_comunicaciones/Interfaces/IAuditoriasComunicacion.cs
@@ -0,0 +1,9 @@
+﻿namespace lib_comunicaciones.Interfaces
+{
+    public interface IAuditoriasComunicacion
+    {
+        Task<Dictionary<string, object>> Listar(Dictionary<string, object> datos);
+        Task<Dictionary<string, object>> Buscar(Dictionary<string, object> datos);
+        Task<Dictionary<string, object>> Guardar(Dictionary<string, object> datos);
+    }
+}
