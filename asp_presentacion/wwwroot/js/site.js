9035cefa09a18ee2dd9e7e09e363ccd1a9be40f0
Sebastián <sebastianjesus.perez@gmail.com>
2024-11-24 16:55:37 -0500

    Se actualiza el diseño del layout y se corrigen los inputs para Auditorías y para Lotes

--- asp_presentacion/wwwroot/js/site.js
+++ asp_presentacion/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    const toggleButton = document.getElementById("sidebar-toggle");
+
+    // Alterna la clase "open" en la barra lateral
+    sidebar.classList.toggle("open");
+
+    // Cambia la posición del botón sincronizado con la barra lateral
+    if (sidebar.classList.contains("open")) {
+        toggleButton.style.left = "270px";
+    } else {
+        toggleButton.style.left = "20px";
+    }
+}
+
