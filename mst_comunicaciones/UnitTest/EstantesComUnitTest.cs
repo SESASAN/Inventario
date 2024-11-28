d596c36755a37d8727d13188096cc285d460d4b9
NathyGaby04 <nathalie040904@gmail.com>
2024-11-27 22:55:29 -0500

    Cambio de la interfaz y mst pruebas comunicaciones

---
+++ mst_comunicaciones/UnitTest/EstantesComUnitTest.cs
@@ -0,0 +1,92 @@
+using lib_entidades.Modelos;
+using lib_comunicaciones;
+using lib_utilidades;
+using lib_comunicaciones.Interfaces;
+using lib_comunicaciones.Implementaciones;
+using mst_comunicaciones.Nucleo;
+
+namespace mst_comunicaciones.UnitTest
+{
+    [TestClass]
+    public class EstantesComUnitTest
+    {
+        private IEstantesComunicacion? iComunicacion = null;
+        private Estantes? entidad = null;
+        private List<Estantes>? lista = null;
+        private static string token = null;
+        private TokenHelper TokenHelper;
+
+
+
+        public EstantesComUnitTest()
+        {
+            iComunicacion = new EstantesComunicacion();
+            TokenHelper = new TokenHelper();
+            var task = TokenHelper.Autenticar();
+            task.Wait();
+            token = task.Result;
+
+        }
+
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            await Listar();
+            await Guardar();
+            await Modificar();
+            await Borrar();
+        }
+
+        private async Task Guardar()
+        {
+            var dicc = new Dictionary<string, object>();
+            entidad = EntidadesHelper.ObtenerEstantes();
+            dicc!["Entidad"] = entidad;
+            dicc!["Token"] = token;
+
+
+            var datos = await iComunicacion!.Guardar(dicc);
+
+            var enti = entidad = JsonConversor.ConvertirAObjeto<Estantes>(
+                JsonConversor.ConvertirAString(datos["Entidad"]));
+            entidad.Id = enti.Id;
+            Assert.IsTrue(!datos.ContainsKey("Error"));
+
+        }
+        private async Task Listar()
+        {
+            var dicc = new Dictionary<string, object>();
+            dicc!["Token"] = token;
+            var datos = await iComunicacion!.Listar(dicc);
+
+            Assert.IsTrue(!datos.ContainsKey("Error"));
+        }
+
+        private async Task Modificar()
+        {
+            var dicc = new Dictionary<string, object>();
+            entidad!.Nombre = entidad.Nombre + " " + DateTime.Now.ToString();
+            dicc!["Entidad"] = entidad;
+            dicc!["Token"] = token;
+
+
+            var datos = await iComunicacion!.Modificar(dicc);
+
+            Assert.IsTrue(!datos.ContainsKey("Error"));
+
+        }
+
+        private async Task Borrar()
+        {
+            var dicc = new Dictionary<string, object>();
+            dicc!["Entidad"] = entidad!;
+            dicc!["Token"] = token;
+
+
+            var datos = await iComunicacion!.Borrar(dicc);
+
+            Assert.IsTrue(!datos.ContainsKey("Error"));
+
+        }
+    }
+}
---
+++ mst_comunicaciones/UnitTest/ProductosComUnitTest.cs
@@ -0,0 +1,92 @@
+using lib_entidades.Modelos;
+using lib_comunicaciones;
+using lib_utilidades;
+using lib_comunicaciones.Interfaces;
+using lib_comunicaciones.Implementaciones;
+using mst_comunicaciones.Nucleo;
+
+namespace mst_comunicaciones.UnitTest
+{
+    [TestClass]
+    public class ProductosComUnitTest
+    {
+        private IProductosComunicacion? iComunicacion = null;
+        private Productos? entidad = null;
+        private List<Productos>? lista = null;
+        private static string token = null;
+        private TokenHelper TokenHelper;
+
+
+
+        public ProductosComUnitTest()
+        {
+            iComunicacion = new ProductosComunicacion();
+            TokenHelper = new TokenHelper();
+            var task = TokenHelper.Autenticar();
+            task.Wait();
+            token = task.Result;
+
+        }
+
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            await Listar();
+            await Guardar();
+            await Modificar();
+            await Borrar();
+        }
+
+        private async Task Guardar()
+        {
+            var dicc = new Dictionary<string, object>();
+            entidad = EntidadesHelper.ObtenerProductos();
+            dicc!["Entidad"] = entidad;
+            dicc!["Token"] = token;
+
+
+            var datos = await iComunicacion!.Guardar(dicc);
+
+            var enti = entidad = JsonConversor.ConvertirAObjeto<Productos>(
+                JsonConversor.ConvertirAString(datos["Entidad"]));
+            entidad.Id = enti.Id;
+            Assert.IsTrue(!datos.ContainsKey("Error"));
+
+        }
+        private async Task Listar()
+        {
+            var dicc = new Dictionary<string, object>();
+            dicc!["Token"] = token;
+            var datos = await iComunicacion!.Listar(dicc);
+
+            Assert.IsTrue(!datos.ContainsKey("Error"));
+        }
+
+        private async Task Modificar()
+        {
+            var dicc = new Dictionary<string, object>();
+            entidad!.Nombre = entidad.Nombre + " " + DateTime.Now.ToString();
+            dicc!["Entidad"] = entidad;
+            dicc!["Token"] = token;
+
+
+            var datos = await iComunicacion!.Modificar(dicc);
+
+            Assert.IsTrue(!datos.ContainsKey("Error"));
+
+        }
+
+        private async Task Borrar()
+        {
+            var dicc = new Dictionary<string, object>();
+            dicc!["Entidad"] = entidad!;
+            dicc!["Token"] = token;
+
+
+            var datos = await iComunicacion!.Borrar(dicc);
+
+            Assert.IsTrue(!datos.ContainsKey("Error"));
+
+        }
+    }
+}
